068ead2508dd2ab51461c8e3bd61a430e4e266b4
superbong19 <congluan19092003@gmail.com>
2023-06-26 20:21:47 +0700

    Testimonial_Component Completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './testimonials.css'
+import AVTAR1 from '../../assets/AVTAR1.jpeg'
+
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
     return (
-        <section id='testimonials'>Testimonials</section>
+        <section id='testimonials'>
+            <h5>Review from client</h5>
+            <h2>Testimonials</h2>
+            <Swiper className="container testimonial__container"
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide className="testimonial">
+                    <div className="client__avatar">
+                        <img src={AVTAR1} alt="Avatar One" />
+                    </div>
+                    <h5 className='client__name'>Ernes Achiever</h5>
+                    <small className='client__review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis soluta eum quae consequuntur laborum enim eligendi voluptatem. Minima, ducimus molestiae accusantium quo quae quis! Quisquam voluptas voluptatum aspernatur dolores ipsum.
+                    </small>
+                </SwiperSlide>
+                <SwiperSlide className="testimonial">
+                    <div className="client__avatar">
+                        <img src={AVTAR1} alt="Avatar One" />
+                    </div>
+                    <h5 className='client__name'>Ernes Achiever</h5>
+                    <small className='client__review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis soluta eum quae consequuntur laborum enim eligendi voluptatem. Minima, ducimus molestiae accusantium quo quae quis! Quisquam voluptas voluptatum aspernatur dolores ipsum.
+                    </small>
+                </SwiperSlide>
+                <SwiperSlide className="testimonial">
+                    <div className="client__avatar">
+                        <img src={AVTAR1} alt="Avatar One" />
+                    </div>
+                    <h5 className='client__name'>Ernes Achiever</h5>
+                    <small className='client__review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis soluta eum quae consequuntur laborum enim eligendi voluptatem. Minima, ducimus molestiae accusantium quo quae quis! Quisquam voluptas voluptatum aspernatur dolores ipsum.
+                    </small>
+                </SwiperSlide>
+            </Swiper>
+        </section>
     )
 }
 
